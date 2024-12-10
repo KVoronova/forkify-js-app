362cbb6343d4bdf5e375b27cf9add6e8ea081104
KVoronova <voronova.vs@gmail.com>
2024-12-10 16:50:29 +0300

    create full version project

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0fa9256e-9ec9-4f76-95f4-6c0ef6830f10';
+export const MODAL_CLOSE_SEC = 2.5;
